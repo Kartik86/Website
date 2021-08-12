e6c93cc6d1a0546ac03b1ddad741556db232a068
Kartik86 <kkaushik491@gmail.com>
2021-08-13 00:32:47 +0100

    Public server connection

--- dbConnector.php
+++ dbConnector.php
@@ -1,11 +1,11 @@
 <?php
 
-$dbServer = "localhost";
-$dbName = "FYP_database";
-$dbPassword = "";
-$dbUsername = "root";
+$dbServer = "sql303.unaux.com";
+$dbName = "unaux_29384808_FYP_database";
+$dbPassword = "gzkx22v4bv";
+$dbUsername = "unaux_29384808";
 
-$con = mysqli_connect($dbServer, $dbUsername, "", $dbName);
+$con = mysqli_connect($dbServer, $dbUsername, $dbPassword, $dbName);
 
 //checking connection if failed
 if($con == false){
